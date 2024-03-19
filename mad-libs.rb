6f49edac1bd248d46ff8d4bf51f2dec1af9ac707
Tega <beniyoke@gmail.com>
2024-03-19 22:41:11 +0100

    Mad libs game

---
+++ mad-libs.rb
@@ -0,0 +1,15 @@
+puts "Welcome to my mad libs game"
+puts "To begin I'll prompt you to enter some few characters"
+
+puts "Enter a color"
+color = gets.chomp
+
+puts "Enter a plural noun"
+noun = gets.chomp
+
+puts "Enter a celebrity name"
+celebrity = gets.chomp
+
+puts "Roses are " +  color
+puts noun.capitalize + " are blue"
+puts "I love " +  celebrity.capitalize
